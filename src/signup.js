2b70596872502ef540e2c256dc35fec93e9acac1
Minna Barry <barrymin@onid.oregonstate.edu>
2015-06-01 18:07:54 -0700

    created signup files and wrote code to validate input

---
+++ src/signup.js
@@ -0,0 +1,107 @@
+function validateUsername(username){
+   if(username.length > 3 && username.length <= 20){
+      if(username.search(" ")=== -1){
+         document.getElementById("warninguser").innerHTML = "";
+         return true;
+      }else{
+		  document.getElementById("warninguser").innerHTML = "username cannot include spaces";
+		  return false;
+	  }
+   }else{
+      document.getElementById("warninguser").innerHTML = "username must be between 3 and 20 characters";
+	  return false;
+   }
+}
+function validatePassword(pass){
+   if(pass.length !== 0){
+      if(pass.length >= 6 && pass.length <= 16){
+         document.getElementById("warningpass").innerHTML = "";
+         return true;
+      }else{
+		  document.getElementById("warningpass").innerHTML = "password must be between 6 and 16 character";
+		  return false;
+	  }
+   }else{
+      document.getElementById("warningpass").innerHTML = "password cannot be empty";
+	  return false;
+   }
+}
+function confirmPassword(){
+   pass = document.getElementById("signup-password").value;
+   pass2 = document.getElementById("signup-password2").value;
+   if(pass === pass2){
+      document.getElementById("warningpass2").innerHTML = "";
+	  return true;
+   }else{
+	  document.getElementById("warningpass2").innerHTML = "Passwords Don't match";
+	  return false;
+   }
+}
+function validateName(name){
+	if(name.length !== 0 && name.charAt(0) !== " "){
+	   document.getElementById("warningname").innerHTML = "";
+       return true;  
+	}else{
+	   document.getElementById("warningname").innerHTML = "Name cannot be empty";
+       return false;
+	}
+}
+function validateEmail(email){
+   if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){  
+      document.getElementById("warningemail").innerHTML = "";
+	  return true;
+   }else {
+	   document.getElementById("warningemail").innerHTML = "Invalid email";
+	   return false;
+   }
+}
+
+/*
+*validateSignup()
+*makes sure username, pass, name and email are valid then sends a request to check 
+*if the username exits and creates a user.
+*/
+function validateSignup() {
+   var username = document.getElementById("signup-username").value;
+   var password = document.getElementById("signup-password").value;
+   var name = document.getElementById("signup-name").value;
+   var email = document.getElementById("signup-email").value;
+   
+   if (validateUsername(username) && validatePassword(password)
+	   && confirmPassword() && validateName(name) && validateEmail(email)){
+      if (window.XMLHttpRequest) {
+         httpRequest = new XMLHttpRequest();
+      }
+      if (!httpRequest) {
+         alert('Cannot create XMLHTTP');
+         return false;
+      }
+      httpRequest.onreadystatechange = function () {
+         if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200) {
+               handleResponse(this.response);
+               if(this.response.length === 0){
+                  //user is created and logged in
+                  location.reload();
+               }
+            }
+         }
+      };
+      httpRequest.open('POST',"http://web.engr.oregonstate.edu/~barrymin/cs290Final/validation.php", true);
+      httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+      httpRequest.send("signup-username="+encodeURIComponent(username)+"&signup-pass="+encodeURIComponent(password)
+	     +"&signup-name="+encodeURIComponent(name)+"&signup-email="+encodeURIComponent(email));
+   }
+   return false;
+}
+/*
+*handleResponse(response)
+*displays the response to the user if it was not empty
+*/
+function handleResponse(response){
+   if(response.length === 0){
+        document.getElementById("warningmsg").innerHTML = "";
+      }else{
+          document.getElementById("warningmsg").innerHTML = response;
+      }
+}
---
+++ src/signup.php
@@ -0,0 +1,74 @@
+<?php
+//redirect to main page if user is logged in
+session_start();
+if(isset($_SESSION["username"])){
+    header('location: http://web.engr.oregonstate.edu/~barrymin/cs290Final/main.php');
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Signup</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+  <script src="signup.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <div class="row">
+  <div class="col-md-4">
+  </div>
+  <div class="col-md-4">
+    <div class="page-header">
+      <h1>Sign Up Page</h1>
+	  <p>please signup</p>
+    </div>
+	<div id="signupspace">
+	  <p id="warningmsg" class="alert-warning">
+	  </p>
+	  <form role="form" onsubmit="return validateSignup()" method="get">
+		  <div class="form-group">
+		    <p id="warninguser" class="alert-warning">
+	        </p>
+		    <label>Username: </label>
+		    <input class="form-control" id="signup-username" type="text" onchange="validateUsername(this.value)" name="user-username" required="required">
+		  </div>
+		  <div class="form-group">
+		    <p id="warningpass" class="alert-warning">
+	        </p>
+		    <label>Password: </label>
+		    <input class="form-control" id="signup-password" type="password" onchange="validatePassword(this.value)" name="user-pass" required="required">
+		  </div>
+		  <div class="form-group">
+		    <p id="warningpass2" class="alert-warning">
+	        </p>
+		    <label>Confirm Password: </label>
+		    <input class="form-control" id="signup-password2" type="password" onchange="confirmPassword()" name="user-pass2" required="required">
+		  </div>
+		  <div class="form-group">
+		    <p id="warningname" class="alert-warning">
+	        </p>
+		    <label>Name: </label>
+		    <input class="form-control" id="signup-name" type="text" onchange="validateName(this.value)" name="user-name" required="required">
+		  </div>
+		  <div class="form-group">
+		    <p id="warningemail" class="alert-warning">
+	        </p>
+		    <label>Email: </label>
+		    <input class="form-control" id="signup-email" type="email" onchange="validateEmail(this.value)" name="user-email" required="required">
+		  </div>
+          <input id="signupbtn" type="submit" value="signup" class="btn btn-default"></input>    
+	  </form>
+	</div>
+    <div class="col-md-4">
+    </div>
+  </div>
+
+  </div>
+</div>
+</body>
+</html>
